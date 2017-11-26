37cc973a2a3fdaedc3de5dae335de2c6f7b9ca6a
CelsoEd <33663150+CelsoEd@users.noreply.github.com>
2017-11-26 12:54:53 -0200

    Update Principal.java

--- Listas/src/lista3/Principal.java
+++ Listas/src/lista3/Principal.java
@@ -7,11 +7,11 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		cartoes = new CartaoWeb[3];
-		DiaDosNamorados cartao1 = new DiaDosNamorados("Fulana");
+		DiaDosNamorados cartao1 = new DiaDosNamorados("Josevalda");
 		cartoes [0] = cartao1;
-		Natal cartao2 = new Natal("Cicrano");
+		Natal cartao2 = new Natal("Jose");
 		cartoes[1] = cartao2;
-		Aniversario cartao3 = new Aniversario("Deltrano");
+		Aniversario cartao3 = new Aniversario("Anderbal");
 		cartoes[2] = cartao3;
 		for(int i = 0; i < cartoes.length; i++) {
 			JOptionPane.showMessageDialog(null, cartoes[i].retornarMensagem("Celso"));
